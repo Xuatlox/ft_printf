32a35a9acb714e8ef395f4ae3cc792aa2c07a765
ansimonn <ansimonn@student.42angouleme.fr>
2025-11-03 12:47:43 +0100

    return value except on %c

--- ft_printf.c
+++ ft_printf.c
@@ -6,66 +6,75 @@
 /*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 11:06:40 by ansimonn          #+#    #+#             */
-/*   Updated: 2025/10/29 17:35:05 by ansimonn         ###   ########.fr       */
+/*   Updated: 2025/11/03 12:42:42 by ansimonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <stdio.h>
 #include "libft/libft.h"
 
-static void	ft_putnbr_unsigned(unsigned int nb)
+static int	ft_putnbr_unsigned(unsigned int nb)
 {
 	char	digit;
+	int		ret;
 
+	ret = 0;
 	if (nb > 9)
 	{
-		ft_putnbr_unsigned(nb / 10);
+		ret += ft_putnbr_unsigned(nb / 10);
 	}
 	digit = nb % 10 + '0';
-	write(1, &digit, 1);
+	if (write(1, &digit, 1) == -1)
+		return (-1);
+	return (ret + 1);
 }
 
-static void	ft_putnbr_hex(unsigned long int nbr, int ind)
+static int	ft_putnbr_hex(unsigned int nbr, int ind)
 {
 	char	conv;
+	int		ret;
+
+	ret = 0;
 	if (ind == 2)
 	{
 		ind = 0;
-		ft_putstr_fd("0x", 1);
-	}
-	if (nbr < 0)
-	{
-		nbr = -nbr;
+		if (ft_putstr_fd("0x", 1) == -1)
+			return (-1);
+		ret += 2;
 	}
 	if (nbr > 16)
-		ft_putnbr_hex(nbr / 16, ind);
+		ret += ft_putnbr_hex(nbr / 16, ind);
 	if (nbr % 16 < 10)
 		conv = '0' + nbr % 16;
 	else if (ind == 1)
 		conv = 'A' + nbr % 16 - 10;
 	else
 		conv = 'a' + nbr % 16 - 10;
-	write(1, &conv, 1);
+	if (write(1, &conv, 1) == -1)
+		return (-1);
+	ret++;
+	return (ret);
 }
 
 static int	ft_conversion(va_list args, const char spec)
 {
 	if (spec == 'c')
-	 	ft_putchar_fd(va_arg(args, int), 1);
-	 else if (spec == 's')
-	 	ft_putstr_fd(va_arg(args, char *), 1);
-	 else if (spec == 'p')
-	 	ft_putnbr_hex((unsigned long int) va_arg(args, void *), 2);
-	 else if (spec == 'd' || spec == 'i')
-	 	ft_putnbr_fd(va_arg(args, int), 1);
-	 else if (spec == 'u')
-	 	ft_putnbr_unsigned(va_arg(args, unsigned int));
-	 else if (spec == 'x')
-	 	ft_putnbr_hex(va_arg(args, int), 0);
-	 else if (spec == 'X')
-	 	ft_putnbr_hex(va_arg(args, int), 1);
-	 else if (spec == '%')
-	 	ft_putchar_fd('%', 1);
+	 	return (write(1, va_arg(args, char *), 1));
+	if (spec == 's')
+	 	return (ft_putstr_fd(va_arg(args, char *), 1));
+	if (spec == 'p')
+	 	return (ft_putnbr_hex(va_arg(args, unsigned int), 2));
+	if (spec == 'd' || spec == 'i')
+	 	return (ft_putnbr_fd(va_arg(args, int), 1));
+	if (spec == 'u')
+	 	return (ft_putnbr_unsigned(va_arg(args, unsigned int)));
+	if (spec == 'x')
+	 	return (ft_putnbr_hex(va_arg(args, int), 0));
+	if (spec == 'X')
+	 	return (ft_putnbr_hex(va_arg(args, int), 1));
+	if (spec == '%')
+	 	return (write(1, "%", 1));
 	return (0);
 }
 
@@ -73,25 +82,34 @@ int ft_printf(const char *format, ...)
 {
 	va_list args;
 	int		ret;
-	int		i;
+	int		tmp;
 
-	i = 0;
 	ret = 0;
 	va_start(args, format);
-	while (format[i])
+	while (*format)
 	{
-		if (format[i] == '%')
+		if (*format == '%')
 		{
 			format++;
-			ret += ft_conversion(args, format[i]);
+			tmp = ft_conversion(args, *format);
+			if (tmp == -1)
+				return (tmp);
+			ret += tmp;
 		}
 		else
 		{
-			ft_putchar_fd(format[i], 1);
+			if (write(1, format, 1) == -1)
+				return (-1);
 			ret++;
 		}
-		i++;
+		format++;
 	}
 	va_end(args);
 	return (ret);
+}
+
+int main()
+{
+	printf("%d\n", printf("%%\n", 42));
+	printf("%d\n", ft_printf("%%\n", 42));
 }
